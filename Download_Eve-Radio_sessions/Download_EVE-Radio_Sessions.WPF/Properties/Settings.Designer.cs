b61cbb80269e7ff6a5515e29f6a7e25b4e6f7461
Drummendejef <jorenvandamme@hotmail.com>
2018-12-28 15:16:42 +0100

    Revert stuff

--- Download_Eve-Radio_sessions/Download_EVE-Radio_Sessions.WPF/Properties/Settings.Designer.cs
+++ Download_Eve-Radio_sessions/Download_EVE-Radio_Sessions.WPF/Properties/Settings.Designer.cs
@@ -8,17 +8,21 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Download_EVE_Radio_Sessions.WPF.Properties {
-    
-    
+namespace Download_EVE_Radio_Sessions.WPF.Properties
+{
+
+
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
-        
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
+    {
+
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-        
-        public static Settings Default {
-            get {
+
+        public static Settings Default
+        {
+            get
+            {
                 return defaultInstance;
             }
         }
